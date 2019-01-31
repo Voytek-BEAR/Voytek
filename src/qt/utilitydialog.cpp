abc351ea8417d78f28827e96d3e553c5b8987165
VoytekBear <44909983+VoytekBear@users.noreply.github.com>
2019-01-31 21:42:23 +0100

    v1.1.0-FakeStakeFix

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -35,7 +35,7 @@ HelpMessageDialog::HelpMessageDialog(QWidget* parent, bool about) : QDialog(pare
     ui->setupUi(this);
     GUIUtil::restoreWindowGeometry("nHelpMessageDialogWindow", this->size(), this);
 
-    QString version = tr("Voytek BEAR") + " " + tr("Version 1.0.0-MAIN");
+    QString version = tr("Voytek BEAR") + " " + tr("Version 1.1.0-MAIN");
 /* On x86 add a bit specifier to the version so that users can distinguish between
      * 32 and 64 bit builds. On other architectures, 32/64 bit may be more ambigious.
      */
